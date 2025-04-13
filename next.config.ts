c6c16eed6b0c429f3cf8d59e8004dbd3d3ff4d9e
Jiwei99 <jwlim99@gmail.com>
2025-04-13 17:51:28 +0800

    Test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/fin4719",
+  assetPrefix: "/fin4719/",
 };
 
 export default nextConfig;
